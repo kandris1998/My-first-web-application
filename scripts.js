a0187d704744033eed16f45d84d04afaf5053df9
Kiss András <andris998@freemail.hu>
2020-06-10 18:36:31 +0200

    day 3

--- scripts.js
+++ scripts.js
@@ -12,20 +12,44 @@ $('#photo1').attr("src", data.photo);
 $("#photo-title").attr("h1", data.title);
 $("#description").attr("text", data.description);
 */
+
 //Day 2 part 2
 
-let currentPhoto = 4 ;
+let currentPhoto = 0 ;
 let imagesData = {
     photo: ["images/photo1.jpg","images/photo2.jpg", "images/photo3.jpg","images/photo4.jpg","images/photo5.jpg"],
     title: ["Róma", "Valagszaggató", "Tesákkal", "Corsica", "Kis Frenki"],
     description: ["description1", "description2", "description 3", "description 4", "description 5"]
-}
+};
 
 
 $('#photo1').attr("src", imagesData.photo[currentPhoto]);
 $('h1#photo-title').text(imagesData.title[currentPhoto]);
 $('p#description').text(imagesData.description[currentPhoto]);
 
+//Day 3 Part 1
+
+let loadPhoto = (photoNumber) => {
+    $('#photo1').attr("src", imagesData.photo[photoNumber]);
+    $('h1#photo-title').text(imagesData.title[photoNumber]);
+    $('p#description').text(imagesData.description[photoNumber]);
+};
+
+loadPhoto(currentPhoto); 
+
+$('#right').click(() => {
+    if(currentPhoto < 5 ) {
+        currentPhoto++;
+    }
+    loadPhoto(currentPhoto);
+}); 
+
+$('#left').click(() => {
+    if (currentPhoto >= 0){
+        currentPhoto--;
+    }
+    loadPhoto(currentPhoto);
+}); 
 
 
 //egyéb
